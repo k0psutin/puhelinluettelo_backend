656506a71cf76f17ab12eb3cb5f199dd79480335
k0psutin <mr.gravez@gmail.com>
2019-09-27 12:22:57 +0300

    uibuild

---
+++ build/precache-manifest.9caecb8dabbad8c47a53a72893681c89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "371cdf5710e69ab75e3590cf33c870d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06eeb55ed7d154cd3833",
+    "url": "/static/css/main.c5990c8b.chunk.css"
+  },
+  {
+    "revision": "d5a877012256dcf7cf19",
+    "url": "/static/js/2.720427c3.chunk.js"
+  },
+  {
+    "revision": "06eeb55ed7d154cd3833",
+    "url": "/static/js/main.ac1bc41f.chunk.js"
+  },
+  {
+    "revision": "64fda7fd07fd25d97db7",
+    "url": "/static/js/runtime-main.11bda596.js"
+  }
+]);
