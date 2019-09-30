0f51baa0f652202ed50bd04a213026810b6b89e3
k0psutin <mr.gravez@gmail.com>
2019-09-30 13:02:06 +0300

    Update 3.14 added MongoDB

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const Person = require('./modules/person')
 
 const stringFormat = ':method :url :status :req[content-length] :response-time ms - :post'
 
@@ -11,92 +13,49 @@ app.use(morgan(stringFormat))
 app.use(cors())
 app.use(express.static('build'))
 
-let persons = [
-    {
-        name: "Ada Lovelace", 
-        number: "040-696969",
-        id: 1
-    },
-    {
-        name: "Arto Hellas", 
-        number: "040-123456",
-        id: 2
-    },
-    {
-        name: "Abraham DeGrell", 
-        number: "040-904949",
-        id: 3
-    },
-    {
-        name: "Mary Poppendieck", 
-        number: "09-9837372",
-        id: 4
-    },
-    {
-        name: "Spede Pasanen", 
-        number: "044-383838",
-        id: 5
-    }
-]
-
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
 
 morgan.token('post', function (req, res) {
-   if(req.body.name | req.body.number) {
+   if(req.body.name != undefined | req.body.number != undefined) {
     return JSON.stringify({ name: req.body.name,number: req.body.number })
    }
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(n => n.id == id)
-    if(person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
-    
+    Person.findById(request.params.id).then(person => {
+        response.json(person.toJSON())
+    })
 })
-
-const generateNumber = () => Math.floor(Math.random() * 99999) + 500
 
 app.post('/api/persons', (request, response) => {
     const newPerson = request.body
 
-    checkPerson = persons.find(person => person.name == newPerson.name)
-
-    if(checkPerson) {
-        if (checkPerson.name === newPerson.name) {
+        if (newPerson.name === undefined | 
+            newPerson.number === undefined) {
             return response.status(400).json({
-                error: `person with that name already exists`
+                error: `name or number missing`
             })
         }
-    }
 
-    if (!newPerson.name | !newPerson.number) {
-        return response.status(400).json({ 
-          error: 'name or number missing' 
-        })
-      }
-
-    const person = {
+    const person = new Person({
         name: newPerson.name,
         number: newPerson.number,
-        id: generateNumber(),
-    }
+    })
 
     //console.log('newPerson', person.id)
 
-    persons = persons.concat(person)
-
-    response.json(person)
+    person.save().then(savedPerson => {
+        response.json(savedPerson.toJSON())
+    })
 })
 
 app.get('/api/persons/', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/api/info/', (request, response) => {
